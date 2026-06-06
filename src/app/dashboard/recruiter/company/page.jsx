f1188f740d51788c2db65d2483fdbdb0d2526643
Israt Jahan Ayshi <israt.ayshi1999@gmail.com>
2026-06-06 21:38:42 +0600

    company add from recruiter and show on dashboard

--- src/app/dashboard/recruiter/company/page.jsx
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import CompanyProfile from './CompanyProfile';
+import { getUserSession } from '@/lib/core/session';
+import { getRecruiterCompany } from '@/lib/api/companies';
 
-const RecruiterCompany = () => {
+
+const CompanyPage = async () => {
+
+    const user = await getUserSession();
+    const company = await getRecruiterCompany(user?.id);
+
     return (
         <div>
-            <h2>Company Profile</h2>
+            <CompanyProfile recruiter={user} recruiterCompany={company}></CompanyProfile>
         </div>
     );
 };
 
-export default RecruiterCompany;
+export default CompanyPage;
+
+//CompanyPage is server side component, thats why we could use async. User side component can't be made async
---
+++ src/lib/core/server.js
@@ -0,0 +1,29 @@
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const serverFetch = async (path) => {
+    const res = await fetch(`${baseUrl}${path}`);
+    // handle 401, 404, 403
+    return res.json();
+}
+
+export const serverMutation = async (path, data) => {
+    const res = await fetch(`${baseUrl}${path}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    // const text = await res.text();
+    // console.log('Raw response:', text); // ← check Next.js terminal
+
+    // if (!text) {
+    //     throw new Error('Empty response from server');
+    // }
+    // handle 401, 404, 403
+    // if (!res.ok) throw new Error(res.statusText);
+
+    return res.json();
+}
